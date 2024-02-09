4450dddd2b432998a695a31d4743df17ef007053
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-09 15:45:42 -0800

    implemented average-pair and can-construct-word

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,35 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/**
+ * inputs: nums (array of numbers), targetAvg (number)
+ * output: true/false
+ * NUM IS SORTED ARRAY
+ *
+ * testcases:
+ * averagePair([1, 2, 3], 2.5);         // true
+ * averagePair([3, 3, 6, 12, 19], 8);   // false
+ * averagePair([1, 2, 3], 2);           // true
+ * averagePair([], 4);                  // false
+ */
+
+function averagePair(nums, targetAvg) {
+  //declare index variables (left=0, right=nums.length-1)
+  let left = 0;
+  let right = nums.length - 1;
+  //while (left < right)
+  while (left < right) {
+    //get avg = (nums[left] + nums[right]) / 2
+    //if avg === targetAvg, return true
+    //if (avg < targetAvg) left++
+    //else right--
+    const avg = (nums[left] + nums[right]) / 2;
+
+    if (avg === targetAvg) return true;
+
+    if (avg < targetAvg) left++;
+    else right--;
+  }
+
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,44 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+/**
+ input: word, letters (both strings)
+ output: true/false
+ ONLY LOWERCASE! NO SPACE OR PUNCTUATION!
+
+ testcases:
+ canConstructWord('aa', 'abc');
+  // false -- can't build "aa" with only 1 "a"
+
+canConstructWord('abc', 'dcba');
+  // true -- can build "abc" with letters "abcd"
+
+canConstructWord('aabb', 'bcabad');
+  // true -- can build "aabbcc" with those letters
+ */
+function canConstructWord(word, letters) {
+  //create freq counter for word
+  const wordCounter = createFrequencyCounter(word);
+  //create freq counter for letters
+  const letterCounter = createFrequencyCounter(letters);
+
+  //loop through word freq counter
+  for (let letter in wordCounter) {
+    //&& check that wordFreq[letter] <= lettersFreq[letter]
+    //if false, return false
+    if (!(letter in letterCounter)) return false;
+    if (wordCounter[letter] > letterCounter[letter]) return false;
+  }
+
+  return true;
+}
+
+function createFrequencyCounter(str) {
+  const letterCounts = {};
+
+  for (let letter of str) {
+    const count = letterCounts[letter] || 0;
+    letterCounts[letter] = count + 1;
+  }
+
+  return letterCounts;
 }
